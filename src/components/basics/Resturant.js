66900f5ba7ae5f4ac8aee92ef52552a874ca18da
vivek90s <ak251997@gmail.com>
2021-08-02 23:46:34 +0530

    added navbar

--- src/components/basics/Resturant.js
+++ src/components/basics/Resturant.js
@@ -7,6 +7,15 @@ const Resturant = () => {
     const [menuData, setMenuData] = useState(Menu);
     return (
         <>
+        <nav className="navbar">
+            <div className="btn-group">
+                <button className="btn-group__item">Breakfast</button>
+                <button className="btn-group__item">lunch</button>
+                <button className="btn-group__item">Evening</button>
+                <button className="btn-group__item">Dinner</button>
+                <button className="btn-group__item">All</button>
+            </div>
+        </nav>
           <MenuCard menuData={menuData}/>
         </>
     )
